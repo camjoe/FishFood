013ede7d8393008aa7814b61962a7a5690c16e7a
- Click this icon to type in your name before commiting <and your email>
2017-04-03 20:21:50 -0700

    added current and fixed boat bob

---
+++ Assets/MyScripts/BoatCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatCollision : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "BoatCollider")
+        {
+
+        }
+    }
+}
---
+++ Assets/MyScripts/MoveTowards.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowards : MonoBehaviour
+{
+
+    public Transform pSpot;
+    public float speed = 4.0f;
+    private bool inWater = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        pSpot = GameObject.FindGameObjectWithTag("CurrentDest").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inWater)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(pSpot.position - transform.position), speed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "BoatCollider")
+        {
+            inWater = false;
+        }
+    }
+
+}
